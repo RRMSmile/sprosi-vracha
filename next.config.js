d09b5db4b58d1180ecd6644ea084a62d3d1d30dd
RRMSmile <ramazanov.r.m@gmail.com>
2025-10-21 13:22:12 +0300

    fix next.config.js for Next.js 15 export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 👈 статическая сборка вместо next export
-  images: { unoptimized: true },
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
   trailingSlash: true,
   reactStrictMode: true
 };
 
 module.exports = nextConfig;
-
